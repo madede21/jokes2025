ddf3530c0619017b0f9e0ec813416f56ee83689e
madede21 <mmarquez39@cps.edu>
2026-02-03 18:13:42 +0000

    fuck

--- main.py
+++ main.py
@@ -4,34 +4,57 @@
 # make this performance task ready for submission
 # To give the user a fun experience hearing knock knock jokes
 
-joke = input("Do you want to hear a joke? ")
-if joke == "no":
-    print("Okay suit yourself!")
-while joke == "yes":
-    print("Great, Let's Play")
-    question = input("Do you want to hear a joke about robbers, tanks, or pencils? ")
-    if question == "robbers":
-        input("Knock Knock ")
-        input("Calder")
-        print("Calder police - I've been robbed!")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "tanks":
-        input("Knock Knock ")
-        input("Tank ")
-        input("You are welcome! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-    elif question == "pencils":
-        input("Knock Knock ")
-        input("Broken pencil ")
-        input("Nevermind, it's pointless! ")
-        joke = input("Do you want to hear another joke or are you finished? ")
-if joke == "finished":
-    rate = int(input("Please rate our game 1-10! "))
-    final_score = int(rate * 10)
-    print(str(final_score) + " percent satisfaction rate")
-    friend = input("Would you recommend this game to a friend? ")
 
-    if friend == "yes" or friend == "maybe":
-        print("Thanks, we appreciate it. ")
-    else:
-        print("Sorry you did not enjoy it. ")
+
+# Knock-knock jokes stored as a list
+
+
+
+
+
+
+all_jokes=[]
+
+def joke_game():
+    jokes = {
+        "robbers": ["Robbers ", "Calder police – I've been robbed!"],
+        "tanks": ["Tanks ", "You are welcome!"],
+        "pencils": ["Pencils ", "Nevermind, it's pointless!"]
+    }
+
+    answer = input("Do you want to hear a joke? ")
+
+    if answer == "no":
+        print("Okay suit yourself!")
+        return
+
+    while answer == "yes":
+        choice = input("Choose: robbers, tanks, or pencils: ")
+
+        if choice in jokes:
+            input("Knock Knock ")
+            input(jokes[choice][0])
+            print(jokes[choice][1])
+        else:
+            print("That’s not an option.")
+
+        answer = input("Do you want to hear another joke or are you finished? ")
+
+    if answer == "finished":
+        rate = int(input("Rate the game 1–10: "))
+        print(str(rate * 10) + "% satisfaction rate")
+
+        friend = input("Would you recommend this game to a friend? ")
+        if friend in ("yes", "no"):
+            print("Thanks, we appreciate it.")
+        else:
+            print("Sorry you did not enjoy it.")
+
+
+
+joke_game()
+all_jokes.append((rate))
+print("\n--- List of all entries ---")
+for entry in all_jokes:
+    print(entry)
+print("-------------------------------\n")
