4ca342a25b678ec345dc1c597265a8fd70a24cd2
madede21 <mmarquez39@cps.edu>
2026-02-05 19:05:14 +0000

    Code

--- main.py
+++ main.py
@@ -1,60 +1,56 @@
 
 
-
-# make this performance task ready for submission
-# To give the user a fun experience hearing knock knock jokes
-
-
-
-# Knock-knock jokes stored as a list
-
-
-
-
-
-
-all_jokes=[]
-
-def joke_game():
-    jokes = {
+all_entries = [] #Empty List
+jokes = {
         "robbers": ["Robbers ", "Calder police – I've been robbed!"],
         "tanks": ["Tanks ", "You are welcome!"],
         "pencils": ["Pencils ", "Nevermind, it's pointless!"]
     }
+def joke_game(player_name):  #Parameter
+    print(f"Welcome, {player_name}") #Argument
 
-    answer = input("Do you want to hear a joke? ")
+
+    answer = input("Do you want to hear a joke? yes or no. ")
 
     if answer == "no":
         print("Okay suit yourself!")
-        return
+        return None, None 
 
     while answer == "yes":
-        choice = input("Choose: robbers, tanks, or pencils: ")
+        choice_question = input("Choose: robbers, tanks, or pencils: ")
 
-        if choice in jokes:
+        if choice_question in jokes:
             input("Knock Knock ")
-            input(jokes[choice][0])
-            print(jokes[choice][1])
+            input(jokes[choice_question][0])
+            print(jokes[choice_question][1])
         else:
             print("That’s not an option.")
 
         answer = input("Do you want to hear another joke or are you finished? ")
 
     if answer == "finished":
-        rate = int(input("Rate the game 1–10: "))
-        print(str(rate * 10) + "% satisfaction rate")
+        rate_game = int(input("Rate the game 1–10: "))
+        print(str(rate_game * 10) + "% satisfaction rate")
 
-        friend = input("Would you recommend this game to a friend? ")
-        if friend in ("yes", "no"):
+        friend_question = input("Would you recommend this game to a friend? ")
+        if friend_question in ("yes", "no"):
             print("Thanks, we appreciate it.")
         else:
             print("Sorry you did not enjoy it.")
 
+        return choice_question, rate_game, friend_question
+
+    return None, None
 
 
-joke_game()
-all_jokes.append((rate))
-print("\n--- List of all entries ---")
-for entry in all_jokes:
+player_name= input("What is your name?")
+choice_question, rate_game, friend_question = joke_game(player_name)
+
+
+if rate_game is not None and friend_question is not None:
+    all_entries.append([choice_question, rate_game, friend_question])
+
+print("\n--- List of all entries so far ---")
+for entry in all_entries:
     print(entry)
-print("-------------------------------\n")
+    print("-------------------------------\n")
--- notes.py
+++ notes.py
@@ -10,3 +10,56 @@
 # Utilize selection (e.g., if/else statements).
 # Use iteration (e.g., loops like for or while).
 # Take different paths based on parameter values.
+all_entries = []
+
+def joke_game():
+    jokes = {
+        "robbers": ["Robbers ", "Calder police – I've been robbed!"],
+        "tanks": ["Tanks ", "You are welcome!"],
+        "pencils": ["Pencils ", "Nevermind, it's pointless!"]
+    }
+
+    answer = input("Do you want to hear a joke? ")
+
+    if answer == "no":
+        print("Okay suit yourself!")
+        return None, None 
+
+    while answer == "yes":
+        choice_question = input("Choose: robbers, tanks, or pencils: ")
+
+        if choice_question in jokes:
+            input("Knock Knock ")
+            input(jokes[choice_question][0])
+            print(jokes[choice_question][1])
+        else:
+            print("That’s not an option.")
+
+        answer = input("Do you want to hear another joke or are you finished? ")
+
+    if answer == "finished":
+        rate_game = int(input("Rate the game 1–10: "))
+        print(str(rate_game * 10) + "% satisfaction rate")
+
+        friend_question = input("Would you recommend this game to a friend? ")
+        if friend_question in ("yes", "no"):
+            print("Thanks, we appreciate it.")
+        else:
+            print("Sorry you did not enjoy it.")
+
+        return rate_game, friend_question
+
+    return None, None
+
+
+
+rate_game, friend_question = joke_game()
+
+
+if rate_game is not None and friend_question is not None:
+    all_entries.append((rate_game, friend_question))
+
+print("\n--- List of all entries so far ---")
+for entry in all_entries:
+    print(entry)
+    print("-------------------------------\n")
